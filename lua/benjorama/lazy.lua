d3368e27386c2cbe1397cf9182f32423277899fc
benjorama <benmacomb@gmail.com>
2023-03-22 22:35:46 -0600

    Adds autoformatting

--- lua/benjorama/lazy.lua
+++ lua/benjorama/lazy.lua
@@ -1,58 +1,62 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable', -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
-	{
-		'catppuccin/nvim',
-		name = 'catppuccin',
-	},
-	{
-		'nvim-treesitter/nvim-treesitter',
-		build = ':TSUpdate'
-	},
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v1.x',
-		dependencies = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{'williamboman/mason.nvim'},           -- Optional
-			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.1',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    config = function()
+      vim.cmd.colorscheme "catppuccin"
+    end
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate'
+  },
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    dependencies = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },             -- Required
+      { 'williamboman/mason.nvim' },           -- Optional
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},         -- Required
-			{'hrsh7th/cmp-nvim-lsp'},     -- Required
-			{'hrsh7th/cmp-buffer'},       -- Optional
-			{'hrsh7th/cmp-path'},         -- Optional
-			{'saadparwaiz1/cmp_luasnip'}, -- Optional
-			{'hrsh7th/cmp-nvim-lua'},     -- Optional
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },         -- Required
+      { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+      { 'hrsh7th/cmp-buffer' },       -- Optional
+      { 'hrsh7th/cmp-path' },         -- Optional
+      { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+      { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},             -- Required
-			{'rafamadriz/friendly-snippets'}, -- Optional
-		}
-	},
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },             -- Required
+      { 'rafamadriz/friendly-snippets' }, -- Optional
+    }
+  },
   {
     'glepnir/lspsaga.nvim',
     event = 'BufRead',
     dependencies = {
-      {'nvim-tree/nvim-web-devicons'},
+      { 'nvim-tree/nvim-web-devicons' },
       --Please make sure you install markdown and markdown_inline parser
-      {'nvim-treesitter/nvim-treesitter'}
+      { 'nvim-treesitter/nvim-treesitter' }
     }
   },
   { 'j-hui/fidget.nvim' },
@@ -63,7 +67,7 @@ require('lazy').setup({
   { 'nvim-lualine/lualine.nvim' },
   { 'lewis6991/gitsigns.nvim' },
   { "lukas-reineke/indent-blankline.nvim" },
-  { 'echasnovski/mini.indentscope', version = '*' },
+  { 'echasnovski/mini.indentscope',       version = '*' },
   { "folke/which-key.nvim" },
-  { 'numToStr/Comment.nvim'},
+  { 'numToStr/Comment.nvim' },
 })
