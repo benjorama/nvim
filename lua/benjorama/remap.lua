fc16dcc97c1354bf02e80e3e68dd5781633661cd
benjorama <benmacomb@gmail.com>
2023-03-25 15:27:44 -0600

    Adds some keymaps

--- lua/benjorama/remap.lua
+++ lua/benjorama/remap.lua
@@ -9,3 +9,14 @@ vim.keymap.set('n', 'H', ':bprevious<CR>')
 -- unload current buffer and delete it from buffer list
 vim.keymap.set('n', 'U', ':bdelete<CR>')
 
+-- simplify window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+
+-- window resizing
+vim.keymap.set('n', '<M-Up>', ':resize -2<CR>')
+vim.keymap.set('n', '<M-Down>', ':resize +2<CR>')
+vim.keymap.set('n', '<M-Left>', ':vertical resize -2<CR>')
+vim.keymap.set('n', '<M-Right>', ':vertical resize +2<CR>')
